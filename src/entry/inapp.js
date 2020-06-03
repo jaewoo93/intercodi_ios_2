39b24f5933ab1f94557c0bef905d4745d29d6769
jwyeyo <jwyeyo@naver.com>
2020-06-03 17:15:12 +0900

    도움말 완료

--- src/entry/inapp.js
+++ src/entry/inapp.js
@@ -29,7 +29,6 @@ export function inappInterfaceGuide () {
     // Localized category names
     gn('orange-block-category-header').textContent = Localization.localize('SMART_BLOCKS');
     gn('yellow-block-category-header').textContent = Localization.localize('HOW_TO_USE');
-    
 
     var blockDescriptionKeys = [
         'LED',
@@ -42,8 +41,16 @@ export function inappInterfaceGuide () {
         'WORD_DESCRIPTION',
         'MOTOR',
         'MOTOR_DESCRIPTION',
-        'SPEAKER_BLOCK',
-        'SPEAKER_BLOCK_DESCRIPTION'
+        'SPEAKER',
+        'SPEAKER_DESCRIPTION',
+        'INFORMATION',
+        'INFO_DESCRIPTION1',
+        'INFO_DESCRIPTION2',
+        'INFO_DESCRIPTION3',
+        'CHARGING',
+        'CHARGING_DESCRIPTION',
+        'SWITCH',
+        'SWITCH_DESCRIPTION'
     ];
 
     for (let i = 0; i < blockDescriptionKeys.length; i++) {
@@ -89,79 +96,6 @@ export function inappPaintEditorGuide () {
         'BLOCKS_TURN_OFF_DESCRIPTION',
         'BLOCKS_SPEAKER',
         'BLOCKS_SPEAKER_DESCRIPTION'
-    ];
-
-    for (let i = 0; i < blockDescriptionKeys.length; i++) {
-        gn(blockDescriptionKeys[i]).textContent = Localization.localize(blockDescriptionKeys[i]);
-    }
-}
-
-export function inappBlocksGuide () {
-    // Localized category names
-    gn('yellow-block-category-header').textContent = Localization.localize('BLOCKS_TRIGGERING_BLOCKS');
-    gn('blue-block-category-header').textContent = Localization.localize('BLOCKS_MOTION_BLOCKS');
-    gn('purple-block-category-header').textContent = Localization.localize('BLOCKS_LOOKS_BLOCKS');
-    gn('green-block-category-header').textContent = Localization.localize('BLOCKS_SOUND_BLOCKS');
-    gn('orange-block-category-header').textContent = Localization.localize('BLOCKS_CONTROL_BLOCKS');
-    gn('red-block-category-header').textContent = Localization.localize('BLOCKS_END_BLOCKS');
-
-    var blockDescriptionKeys = [
-        'BLOCKS_GREEN_FLAG',
-        'BLOCKS_GREEN_FLAG_DESCRIPTION',
-        'BLOCKS_ON_TAP',
-        'BLOCKS_ON_TAP_DESCRIPTION',
-        'BLOCKS_ON_TOUCH',
-        'BLOCKS_ON_TOUCH_DESCRIPTION',
-        'BLOCKS_ON_MESSAGE',
-        'BLOCKS_ON_MESSAGE_DESCRIPTION',
-        'BLOCKS_SEND_MESSAGE',
-        'BLOCKS_SEND_MESSAGE_DESCRIPTION',
-        'BLOCKS_MOVE_RIGHT',
-        'BLOCKS_MOVE_RIGHT_DESCRIPTION',
-        'BLOCKS_MOVE_LEFT',
-        'BLOCKS_MOVE_LEFT_DESCRIPTION',
-        'BLOCKS_MOVE_UP',
-        'BLOCKS_MOVE_UP_DESCRIPTION',
-        'BLOCKS_MOVE_DOWN',
-        'BLOCKS_MOVE_DOWN_DESCRIPTION',
-        'BLOCKS_TURN_RIGHT',
-        'BLOCKS_TURN_RIGHT_DESCRIPTION',
-        'BLOCKS_TURN_LEFT',
-        'BLOCKS_TURN_LEFT_DESCRIPTION',
-        'BLOCKS_HOP',
-        'BLOCKS_HOP_DESCRIPTION',
-        'BLOCKS_GO_HOME',
-        'BLOCKS_GO_HOME_DESCRIPTION',
-        'BLOCKS_SAY',
-        'BLOCKS_SAY_DESCRIPTION',
-        'BLOCKS_GROW',
-        'BLOCKS_GROW_DESCRIPTION',
-        'BLOCKS_SHRINK',
-        'BLOCKS_SHRINK_DESCRIPTION',
-        'BLOCKS_RESET_SIZE',
-        'BLOCKS_RESET_SIZE_DESCRIPTION',
-        'BLOCKS_HIDE',
-        'BLOCKS_HIDE_DESCRIPTION',
-        'BLOCKS_SHOW',
-        'BLOCKS_SHOW_DESCRIPTION',
-        'BLOCKS_POP',
-        'BLOCKS_POP_DESCRIPTION',
-        'BLOCKS_PLAY_RECORDED',
-        'BLOCKS_PLAY_RECORDED_DESCRIPTION',
-        'BLOCKS_WAIT',
-        'BLOCKS_WAIT_DESCRIPTION',
-        'BLOCKS_STOP',
-        'BLOCKS_STOP_DESCRIPTION',
-        'BLOCKS_SET_SPEED',
-        'BLOCKS_SET_SPEED_DESCRIPTION',
-        'BLOCKS_REPEAT',
-        'BLOCKS_REPEAT_DESCRIPTION',
-        'BLOCKS_END',
-        'BLOCKS_END_DESCRIPTION',
-        'BLOCKS_REPEAT_FOREVER',
-        'BLOCKS_REPEAT_FOREVER_DESCRIPTION',
-        'BLOCKS_GO_TO_PAGE',
-        'BLOCKS_GO_TO_PAGE_DESCRIPTION'
     ];
 
     for (let i = 0; i < blockDescriptionKeys.length; i++) {
